cafe61e5c2ad34e40f4505b3bb0aebcd35b7e3b3
Kabir Ahuja <f2015791p@alumni.bits-pilani.ac.in>
2023-02-07 20:20:23 +0000

    Added new prompts and translate test

--- mega/eval_xnli.py
+++ mega/eval_xnli.py
@@ -8,16 +8,16 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import openai
-import datasets
-from datasets import load_dataset
+from datasets import Dataset, DatasetDict, load_dataset, load_from_disk
 from promptsource.templates import DatasetTemplates, Template
+from mega.utils.translator_utils import translate_xnli
 import pdb
 
 openai.api_base = "https://gpttesting1.openai.azure.com/"
 openai.api_type = "azure"
 openai.api_version = "2022-12-01"  # this may change in the future
 
-with open("openai_key.txt") as f:
+with open("keys/openai_key.txt") as f:
     openai.api_key = f.read().split("\n")[0]
 
 
@@ -76,20 +76,40 @@ def parse_args(args: list) -> argparse.Namespace:
     parser.add_argument(
         "--save_dir", default="results", type=str, help="Path to store results"
     )
+    parser.add_argument(
+        "--translate-test", action="store_true", help = "Whether to use translated test data"
+    )
+    parser.add_argument(
+        "--use-val-to-prompt", action="store_true", help = "Whether to use Validation Data for in-context examples"
+    )
     return parser.parse_args(args)
 
 
-def load_xnli_dataset(lang: str) -> Union[datasets.Dataset, datasets.DatasetDict]:
+def load_xnli_dataset(lang: str) -> Union[Dataset, DatasetDict]:
     """
     Args:
         lang (str): Language for which xnli dataset is to be loaded
 
     Returns:
-        Union[datasets.Dataset, datasets.DatasetDict]: huggingface dataset object
+        Union[Dataset, DatasetDict]: huggingface dataset object
     """
     return load_dataset("xnli", lang)
 
+def load_translate_test(tgt_lang: str, pivot_lang: str = "en", test_dataset : Optional[Dataset] = None, data_dir: str = "data") -> Dataset:
 
+    tt_dir = os.path.join(
+        data_dir, "xnli", "translate_test", f"{tgt_lang}_{pivot_lang}"
+    )
+    if not os.path.exists(f"{tt_dir}/dataset_info.json"):
+        if test_dataset is None:
+            raise ValueError("Need to provide `test_dataset`, if translate_test dataset do not exist already")
+        tt_dataset = translate_xnli(test_dataset, tgt_lang, pivot_lang, save_path = tt_dir)
+    else:
+        tt_dataset = load_from_disk(tt_dir)
+    
+    return tt_dataset
+    
+    
 def load_prompt_template(lang: str, prompt_name: str) -> Template:
     """Loads prompt template from promptsource
 
@@ -105,12 +125,12 @@ def load_prompt_template(lang: str, prompt_name: str) -> Template:
 
 
 def choose_few_shot_examples(
-    train_dataset: datasets.Dataset, few_shot_size: int, selection_criteria: int
+    train_dataset: Dataset, few_shot_size: int, selection_criteria: int
 ) -> List[Dict[str, Union[str, int]]]:
     """Selects few-shot examples from training datasets
 
     Args:
-        train_dataset (datasets.Dataset): Training Dataset
+        train_dataset (Dataset): Training Dataset
         few_shot_size (int): Number of few-shot examples
         selection_criteria (few_shot_selection): How to select few-shot examples. Choices: [random, first_k]
 
@@ -171,20 +191,28 @@ def get_model_pred(
     prompt_input, label = construct_prompt(
         train_examples, test_example, train_prompt_template, test_prompt_template
     )
-    response = openai.Completion.create(
-        engine=model,
-        prompt=prompt_input,
-        max_tokens=10,
-        temperature=1,
-        top_p=0.5,
-        logprobs=10,
-    )
+    
+    # Hit the api repeatedly till response is obtained
+    while True:
+        try:        
+            response = openai.Completion.create(
+                engine=model,
+                prompt=prompt_input,
+                max_tokens=10,
+                temperature=1,
+                top_p=0.5,
+                logprobs=10,
+            )
+            break
+        except openai.error.APIConnectionError:
+            continue
+            
     return {"prediction": response["choices"][0]["text"].strip(), "ground_truth": label}
 
 
 def evaluate_model(
-    train_dataset: datasets.Dataset,
-    test_dataset: datasets.Dataset,
+    train_dataset: Dataset,
+    test_dataset: Dataset,
     train_prompt_template: Template,
     test_prompt_template: Template,
     model: str,
@@ -197,8 +225,8 @@ def evaluate_model(
     ToDo: Find alternatives to exact match (embeddings?)
 
     Args:
-        train_dataset (datasets.Dataset): _description_
-        test_dataset (datasets.Dataset): _description_
+        train_dataset (Dataset): _description_
+        test_dataset (Dataset): _description_
         train_prompt_template (Template): _description_
         test_prompt_template (Template): _description_
         model (str): _description_
@@ -254,8 +282,14 @@ def main():
     np.random.seed(args.seed)
 
     # Load datasets for pivot and target languages
-    train_dataset = load_xnli_dataset(args.pivot_lang)["train"]
-    test_dataset = load_xnli_dataset(args.tgt_lang)["validation"]
+    train_dataset = load_xnli_dataset(args.pivot_lang)
+    if not args.use_val_to_prompt:
+        train_dataset = train_dataset["train"]
+    else:
+        train_dataset = train_dataset["validation"]
+    test_dataset = load_xnli_dataset(args.tgt_lang)["test"]
+    if args.translate_test:
+        test_dataset = load_translate_test(args.tgt_lang, args.pivot_lang, test_dataset, data_dir = "data")
 
     # Load prompt templates for train and test datasets
     train_prompt_template = load_prompt_template(
@@ -267,8 +301,12 @@ def main():
         train_dataset, args.few_shot_k, args.few_shot_selection
     )
 
-    out_dir = f"{args.save_dir}/xnli/{args.model}/{args.tgt_lang}/PivotLang_{args.pivot_lang}_PromptName_{args.tgt_prompt_name}_FewShotK_{args.few_shot_k}"
-
+    out_dir = f"{args.save_dir}/xnli/{args.model}/{args.tgt_lang}/PivotLang_{args.pivot_lang}_PromptName_{args.tgt_prompt_name.replace('/','_')}_FewShotK_{args.few_shot_k}"
+    if args.translate_test:
+        out_dir = f"{out_dir}_translate_test"
+    if args.use_val_to_prompt:
+        out_dir = f"{out_dir}_use_val_to_prompt"
+    
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
@@ -285,8 +323,10 @@ def main():
     )
     print(accuracy)
     # Store results
-    with open(f"{out_dir}/metrics.json", "w") as f:
-        json.dump({"accuracy": accuracy}, f, indent=4)
+    results_dict = vars(args)
+    results_dict["metrics"] = {"accuracy": accuracy}
+    with open(f"{out_dir}/results.json", "w") as f:
+        json.dump(results_dict, f, indent=4)
 
 
 if __name__ == "__main__":
---
+++ mega/utils/translator_utils.py
@@ -0,0 +1,90 @@
+import os
+from tqdm import tqdm
+import pdb
+import requests, uuid, json
+from typing import Union, Optional
+import copy
+from datasets import Dataset, load_dataset
+
+# Translator setup for bing
+endpoint = "https://api.cognitive.microsofttranslator.com/"
+with open("keys/bing_translate_key.txt") as f:
+    subscription_key = f.read().split("\n")[0]
+
+# Add your location, also known as region. The default is global.
+# This is required if using a Cognitive Services resource.
+location = "centralindia"
+path = "/translate?api-version=3.0"
+constructed_url = endpoint + path
+
+headers = {
+    "Ocp-Apim-Subscription-Key": subscription_key,
+    "Content-type": "application/json",
+    "X-ClientTraceId": str(uuid.uuid4()),
+}
+
+
+def translate_with_bing(text: str, src: str, dest: str) -> str:
+    """Uses the bing translator to translate `text` from `src` language to `dest` language
+
+    Args:
+        text (str): Text to translate
+        src (str): Source language to translate from
+        dest (str): Language to translate to
+
+    Returns:
+        str: Translated text
+    """
+    params = {"api-version": "3.0", "from": src, "to": [dest]}
+    body = [{"text": text}]
+    
+    try:
+        request = requests.post(
+            constructed_url, params=params, headers=headers, json=body
+        )
+        response = request.json()
+        # pdb.set_trace()
+        translation = response[0]["translations"][0]["text"]
+    except:
+        pdb.set_trace()
+        translation = "<MT Failed>"
+
+    return translation
+
+
+def translate_xnli(
+    xnli_dataset: Dataset, src: str, dest: str, save_path: Optional[str] = None
+) -> Dataset:
+    """Translate premise and hypothesis of xnli dataset
+
+    Args:
+        xnli_dataset (Dataset): Some split (train, test, val) of XNLI dataset
+        src (str): Source language to translate from
+        dest (str): Language to translate to
+        save_path (str, optional): Path to store translated dataset. Doesn't store if set to None. Defaults to None.
+
+    Returns:
+        Dataset: Translated Dataset
+    """
+    
+    # Translate premise
+    xnli_dataset = xnli_dataset.map(
+        lambda example: {"premise": translate_with_bing(example["premise"], src, dest)},
+        num_proc=4,
+    )
+
+    # Translate hypothesis
+    xnli_dataset = xnli_dataset.map(
+        lambda example: {
+            "hypothesis": translate_with_bing(example["hypothesis"], src, dest)
+        },
+        num_proc=4,
+    )
+
+    if save_path is not None:
+        save_dir, _ = os.path.split(save_path)
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        xnli_dataset.save_to_disk(save_path)
+        
+    return xnli_dataset
