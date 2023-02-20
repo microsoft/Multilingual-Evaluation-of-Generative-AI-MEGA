8cfdf42036a9a4c99e93fa11a1b78e291836b7c4
Kabir Ahuja <f2015791p@alumni.bits-pilani.ac.in>
2023-02-20 08:46:18 +0000

    Added compile_xnli_results.py

---
+++ mega/compile_xnli_results.py
@@ -0,0 +1,33 @@
+import glob
+import json
+import pandas as pd
+from tqdm import tqdm
+def get_results(filename):
+    with open(filename) as f:
+        results = json.load(f)
+    prompt_setting = ""
+    if results["pivot_lang"] == results["tgt_lang"]:
+        prompt_setting = "Monolingual"
+    else:
+        if results["translate_test"]:
+            prompt_setting = "Translate Test"
+        else:
+            prompt_setting = "Zero-Shot Cross Lingual"
+    return {
+        "Model": results["model"],
+        "Language": results["tgt_lang"],
+        "Prompt Setting": prompt_setting,
+        "Prompt Type": results["tgt_prompt_name"],
+        "# Few-shot Examples": results["few_shot_k"],
+        "Accuracy": results["metrics"]["accuracy"]
+    }
+
+if __name__ == "__main__":
+    model = "DaVinci003"
+    filenames = glob.glob(f"results/xnli/{model}/**/**/*.json")
+    result_rows = []
+    for filename in tqdm(filenames):
+        result_rows.append(get_results(filename))
+
+    results_final = pd.DataFrame(result_rows)
+    results_final.to_csv("results/xnli_final.csv")
