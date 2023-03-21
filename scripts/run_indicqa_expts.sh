5aa884412f35bec9bc72384400291daa67f95d0b
t-jainprachi <t-jainprachi@microsoft.com>
2023-03-21 12:55:09 +0000

    indicqa eval acript added

--- scripts/run_indicqa_expts.sh
+++ scripts/run_indicqa_expts.sh
@@ -6,7 +6,7 @@ do
     for lang in as bn gu hi kn ml mr or pa ta te
     do
         echo "Running for lang $lang and k $k"
-        python -m mega.eval_qa_gptindex -p en -t $lang -d indicqa --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" --eval_on_val -k $k --short_contexts
+        python -m mega.eval_qa_gptindex -p $lang -t $lang -d indicqa --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" --eval_on_val -k $k --short_contexts
     done
 done
 
@@ -16,7 +16,8 @@ do
     for lang in as bn gu hi kn ml mr or pa ta te
     do
         echo "Running for lang $lang and k $k"
-        python -m mega.eval_qa_gptindex -p en -t $lang -d xquad --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" --eval_on_val -k $k
+        python -m mega.eval_qa_gptindex -p $lang -t $lang -d xquad --pivot_prompt_name "answer_given_context_and_question" --tgt_prompt_name "answer_given_context_and_question" --eval_on_val -k $k
     done
 done
+
 
