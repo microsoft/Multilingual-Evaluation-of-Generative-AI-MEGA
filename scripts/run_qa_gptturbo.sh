59257ce02aecdff4c13e2eb38a6a1f3a7f9b0c47
Sanchit Ahuja <t-sahuja@microsoft.com>
2023-09-20 11:53:34 +0000

    typo

--- scripts/run_qa_gptturbo.sh
+++ scripts/run_qa_gptturbo.sh
@@ -4,5 +4,5 @@ echo "Evaluating for IndicQA"
 for lang in hi as bn gu kn ml mr or pa ta te
 do
     echo "Running for language $lang"
-    python -m mega.eval_qa_gptindex -p en -t $lang --pivot_prompt_name "answer_given_context_and_question+unaswerable" --tgt_prompt_name "answer_given_context_and_question+unaswerable" -k 4 --model "text-davinci-003" -e gpt4v2 --temperature 0 --num_evals_per_sec 2 --eval_on_val --chat-prompt -d indicqa
+    python -m mega.eval_qa_gptindex -p en -t $lang --pivot_prompt_name "answer_given_context_and_question+unaswerable" --tgt_prompt_name "answer_given_context_and_question+unaswerable" -k 4 --model "gpt-35-turbo" -e gpt4v2 --temperature 0 --num_evals_per_sec 2 --eval_on_val --chat-prompt -d indicqa
 done
